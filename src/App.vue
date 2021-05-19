626feb04a338e100e18074bfab2ec9aca3e61624
ranhaodong <ranhaodong1997@gmail.com>
2021-05-19 17:40:54 +0800

    改点样式

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
 <template>
-    <ElHeader class="font-black mt-1">
-      智能人群报监控系统
-    </ElHeader>
-    <ElContainer>
-      <ElAside>
-        <ElMenu>
-          <div v-for="(page,key) in routes" :key="key">
-            <router-link :to="page.path" v-if="page.children" :index="key + ''">
-              <ElSubmenu >
-                <template v-slot:title>
-                  {{ page.name }}
-                </template>
-                <ElMenuItem v-for="(subPage, k) in page.children" :key="k" :index="`${key}-${k}`">
-                  <router-link :to="subPage.path"> {{ subPage.name }}</router-link>
-                </ElMenuItem>
-              </ElSubmenu>
-            </router-link>
-
-            <router-link v-else :to="page.path">
-              <ElMenuItem :index="key+''">
+  <ElHeader class="font-black mt-1">
+    智能人群报监控系统
+  </ElHeader>
+  <ElContainer>
+    <ElAside>
+      <ElMenu>
+        <div v-for="(page,key) in routes" :key="key">
+          <router-link :to="page.path" v-if="page.children" >
+            <ElSubmenu :index="key + ''">
+              <template v-slot:title>
                 {{ page.name }}
+              </template>
+              <ElMenuItem v-for="(subPage, k) in page.children" :key="k" :index="`${key}-${k}`">
+                <router-link :to="subPage.path">
+                  {{ subPage.name }}
+                </router-link>
               </ElMenuItem>
-            </router-link>
-          </div>
-        </ElMenu>
-      </ElAside>
-      <ElMain>
-        <router-view/>
-      </ElMain>
-    </ElContainer>
+            </ElSubmenu>
+          </router-link>
+
+          <router-link v-else :to="page.path">
+            <ElMenuItem :index="key+''">
+              {{ page.name }}
+            </ElMenuItem>
+          </router-link>
+        </div>
+      </ElMenu>
+    </ElAside>
+    <ElMain class="border-2 border-indigo-50">
+      <router-view/>
+    </ElMain>
+  </ElContainer>
 
 </template>
 
--- src/routers/home.vue
+++ src/routers/home.vue
@@ -1,16 +1,38 @@
 <template>
-  <div v-for="i in 4" :key="i" class="mb-3">
-    <img :src="imgPath" alt="">
+  <div v-for="i in dataList" :key="i" class="mb-3 flex">
+    <img :src="imgPath" alt="" class="w-1/2 mr-1">
+    <div>
+      <p>地点:{{ i.time }}</p>
+      <p>人数:{{ i.num }}</p>
+      <p>时间:{{ i.time }}</p>
+    </div>
   </div>
 </template>
 
 <script>
 const imgPath = require("../assets/zhiyuanlou.png")
+import {reactive} from "vue";
+
 
 export default {
   name: "home",
   setup() {
-    return {imgPath}
+    const dataList = reactive([
+      {
+        location: "致远楼",
+        num: 13,
+        time: "2021-3-21"
+      }, {
+        location: "教学楼",
+        num: 160,
+        time: "2021-3-21"
+      }, {
+        location: "图书楼",
+        num: 176,
+        time: "2021-3-21"
+      },
+    ])
+    return {imgPath, dataList}
   }
 }
 </script>
