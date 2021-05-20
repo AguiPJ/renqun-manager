12719f681cc80bc14485cfa25e63cbf17cb398d5
ranhaodong <ranhaodong1997@gmail.com>
2021-05-20 12:10:30 +0800

    + 新增接收管理的table

--- src/routers/jieshou.vue
+++ src/routers/jieshou.vue
@@ -1,10 +1,43 @@
 <template>
-<h2>接受管理</h2>
+  <ElTable :data="data" class="w-full">
+    <ElTableColumn v-for="item in col" :prop="item.prop" :label="item.name" width="180" :key="item"/>
+    <ElTableColumn>
+      <template v-slot>
+        <button @click="deleteCol">编辑</button>
+        <button>删除</button>
+      </template>
+    </ElTableColumn>
+  </ElTable>
 </template>
 
 <script>
+import {ElTable, ElTableColumn} from "element-plus";
+
+const col = [
+  {name:"手机号码",prop:"phoneNumber",width:180},
+  {name:"姓名",prop:"userName",width:180},
+  {name:"接收状态",prop:"status",width:180},
+  {name:"创建时间",prop:"createTime",width:180},
+  {name:"更新时间",prop:"updateTime",width:180},
+  {name:"操作",prop:"doSome",width:180},
+]
+
 export default {
-  name: "jieshou"
+  name: "jieshou",
+  components:{ElTable,ElTableColumn},
+  setup(){
+    const data = [
+      {phoneNumber:'123',userName:"tom", status:"接收中", createTime:"2020-5-20 09:45",updateTime:"2020-5-20 09:45"},
+      {phoneNumber:'123',userName:"may", status:"接收中", createTime:"2020-5-20 09:45",updateTime:"2020-5-20 09:45"},
+      {phoneNumber:'123',userName:"ana", status:"接收中", createTime:"2020-5-20 09:45",updateTime:"2020-5-20 09:45"},
+      {phoneNumber:'123',userName:"foo", status:"接收中", createTime:"2020-5-20 09:45",updateTime:"2020-5-20 09:45"},
+    ]
+
+    function deleteCol(index) {
+      console.log(index)
+    }
+    return {data, col, deleteCol}
+  }
 }
 </script>
 
